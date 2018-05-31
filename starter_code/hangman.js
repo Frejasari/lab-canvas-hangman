baf4d240ba54db0b50b951f82078d10552a55de2
Sari <freja.sender@gmail.com>
2018-05-31 12:44:57 +0200

    create logic of hangman

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,47 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["jasmine", "maxence", "ironhack"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function() {
+  this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+  return this.secretWord;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  return 65 <= keyCode && keyCode <= 90;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function(key) {
+  // var char = String.fromCharCode(key);
+  return !this.letters.includes(key);
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function(index) {
+  this.guessedLetter += this.secretWord[index].toUpperCase();
+  return this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function(letter) {
+  this.errorsLeft--;
+  return this.checkGameOver();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function() {
+  return this.errorsLeft === 0;
+};
 
-// };
+Hangman.prototype.checkWinner = function() {
+  return this.guessedLetter.length === this.secretWord.length;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
-
-document.onkeydown = function (e) {
-
-};
+document.onkeydown = function(e) {};
