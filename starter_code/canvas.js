bc5397b0c514d8f8c2581b8c922dc2c256d51cb5
Sari <freja.sender@gmail.com>
2018-05-31 13:20:09 +0200

    format canvas.js

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,25 @@
-
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  this.canvas = document.getElementById("hangman");
+  this.ctx = this.canvas.getContext("2d");
+  this.secretWord = secretWord;
+  this.offset = this.canvas.width / 7;
+  this.unit = this.canvas.width / 100;
 }
 
-HangmanCanvas.prototype.createBoard = function () {
-
+HangmanCanvas.prototype.createBoard = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
 HangmanCanvas.prototype.drawLines = function () {
 
 };
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+HangmanCanvas.prototype.writeCorrectLetter = function(index) {};
 
-};
+HangmanCanvas.prototype.writeWrongLetter = function(letter, errorsLeft) {};
 
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+HangmanCanvas.prototype.drawHangman = function(shape) {};
 
-};
+HangmanCanvas.prototype.gameOver = function() {};
 
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+HangmanCanvas.prototype.winner = function() {};
