84c08be8a59b7188197d86b9e5bb3d7cfbbd95b4
Sari <freja.sender@gmail.com>
2018-05-31 13:30:27 +0200

    add CoordinatePair for the LetterSpaces

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -4,23 +4,31 @@ function HangmanCanvas(secretWord) {
   this.secretWord = secretWord;
   this.offset = this.canvas.width / 7;
   this.unit = this.canvas.width / 100;
+  this.letterSpaceCoordinates = [];
 }
 
 HangmanCanvas.prototype.createBoard = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
+function CoordinatePair(xStart, xEnd, y) {
+  this.xStart = xStart;
+  this.xEnd = xEnd;
+  this.y = y;
+}
+
 HangmanCanvas.prototype.drawLines = function() {
   var letterSpace = this.unit * 2;
   var gapSpace = this.unit;
+  var y = this.canvas.height - 100;
   for (var i = 0; i < this.secretWord.length; i++) {
     var offset = this.unit * 5;
     var startX = (letterSpace + gapSpace) * i;
     var endX = startX + letterSpace;
-    console.log("startX: " + startX + " endX: " + endX + " i: " + i);
+    this.letterSpaceCoordinates.push(new CoordinatePair(startX, endX, y));
     this.ctx.beginPath();
-    this.ctx.moveTo(startX, this.canvas.height - 100);
-    this.ctx.lineTo(endX, this.canvas.height - 100);
+    this.ctx.moveTo(startX, y);
+    this.ctx.lineTo(endX, y);
     this.ctx.closePath();
     this.ctx.stroke();
   }
